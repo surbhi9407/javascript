50871f50309e68379e08420fcbf8cf6b35649356
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-03-24 09:32:34 +0000

    converstion of datatypes

---
+++ basics_1/conversionoperation.js
@@ -0,0 +1,19 @@
+let score="surbhi"
+console.log(typeof score);
+console.log(typeof(score));
+
+let inNumbervalue = Number(score)
+console.log(typeof inNumbervalue);
+console.log(inNumbervalue);
+
+
+let  isNumber= ""
+let isBooleanvalue= Boolean(isNumber)
+console.log(isBooleanvalue);
+console.log(typeof isBooleanvalue);
+
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
