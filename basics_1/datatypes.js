cf85efacbca8712b9081cce587bf7dfc1c5b3615
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-03-24 08:38:36 +0000

    datatypes

---
+++ basics_1/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node  js not browser
+
+let name="surbhi"
+let age=30
+let islogIn = true
+
+//null //  standalone value 
+console.log(typeof null);
+// undefine  
+console.log(typeof undefine);
