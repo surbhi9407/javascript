b96b66b20d5e7da45403c043c008ac98852ff22e
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-04-11 11:29:23 +0000

    string

---
+++ basics_1/string.js
@@ -0,0 +1,23 @@
+const name="surbhi"
+//console.log(name)
+const repoCount =50
+//console.log(name+repoCount+"value")
+
+console.log(`heloo my name is ${name} and my repoCount is ${repoCount}`)
+
+const gameName=new String('surbhidh')
+console.log(gameName)
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase)
+console.log(gameName.substring(8,4))
+console.log(gameName.slice(-8,4))
+console.log(gameName.trim())
+
+
+
+
+
+
+
+
