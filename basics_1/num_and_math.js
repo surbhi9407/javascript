f4a3362f0aad42362f8d82adf6dce32365c36265
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-04-12 11:40:51 +0000

    num_and_math

---
+++ basics_1/num_and_math.js
@@ -0,0 +1,23 @@
+const score= 200
+const balancee= new Number(200)
+const balance=new Number(23.8966)
+const balance1=new Number(1000000)
+console.log(score)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+console.log(balance1.toLocaleString(`en-IN`))
+console.log(balance.toPrecision())
+
+console.log(Math.abs(-1))
+console.log(Math.round(3.2))
+console.log(Math.ceil(2.1))
+console.log(Math.floor(3.8))
+console.log(Math.max(2,7,4,9,11))
+console.log(Math.min(2,3,45,6,7))
+console.log(Math.random())
+console.log(Math.random()+10)
+max=20
+min=10
+console.log(Math.random()*(max-min+1))+ min
+
