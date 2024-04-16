b49f7d3e65438e0d7da4135cf191a430f1894f8a
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-04-16 17:32:19 +0000

    arrays

---
+++ basics_2/array.js
@@ -0,0 +1,20 @@
+const array=[0,1,2,3,4]
+const myhero=["papa","mummy","bhai","behn"]
+const myarr2=new Array(1,2,3,4)
+console.log(myarr2[1]);
+
+console.log(array);
+array.push(8);
+array.push(9);
+array.push(10);
+array.push(11);
+
+console.log(array);
+array.pop()
+console.log(array);
+array.unshift(1)
+console.log(array);
+array.shift()
+console.log(array);
+console.log(array.includes(9));
+console.log(array.indexOf(9));
