a0e2f136b46e39a7228bd419f6914db8615011d2
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-05-05 10:41:21 +0000

    switch

---
+++ controlflow/switch.js
@@ -0,0 +1,19 @@
+const month= 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
