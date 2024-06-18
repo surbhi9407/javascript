8b1676fd614f59c6358543799af9f2913f85209c
Surbhi_Dhamde <129750812+surbhi9407@users.noreply.github.com>
2024-06-18 10:27:36 +0000

    default_prevent

--- default_prevent/one.js
+++ default_prevent/one.js
@@ -8,6 +8,7 @@ function logClick(event){
         log.innerText=event.defaultPrevented ? `Sorry , but you cannot visit this link!\n${log.innerText}` : `Visiting link...\n${log.innerText}`
     }
 }
+
 const a=document.getElementById("link2");
 a.addEventListener("click",stoplink);
 document.addEventListener("click",logClick);
